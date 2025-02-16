014e9c73491ddd63eee001fe56934adcb3758515
sinvalfelisberto <sinvalfelisberto@gmail.com>
2025-02-16 18:57:41 -0300

    initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  template: `
+    <h2>
+      home works!
+    </h2>
+  `,
+  styles: ``
+})
+export class HomeComponent {
+
+}
