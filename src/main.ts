f89bdee8576cd73e6b072d30d9cffa4b2930cdf8
sinvalfelisberto <sinvalfelisberto@gmail.com>
2025-02-24 08:10:40 -0300

    fixing routes && create details view

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
-/*
- *  Protractor support is deprecated in Angular.
- *  Protractor is used in this example for compatibility with Angular documentation tools.
- */
-import {bootstrapApplication, provideProtractorTestingSupport} from '@angular/platform-browser';
-import {AppComponent} from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routeConfig } from './app/routes';
 
-bootstrapApplication(AppComponent, {providers: [provideProtractorTestingSupport()]}).catch((err) =>
-  console.error(err),
-);
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routeConfig)],
+}).catch((err) => console.error('Error bootstrapping application', err));
